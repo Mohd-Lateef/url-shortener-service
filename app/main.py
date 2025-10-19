4c26db2d83818d62b0710ad038711b3a61608197
Mohd-Lateef <lateefm283@gmail.com>
2025-10-19 22:39:22 +0530

    Entry point

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from crud import create_url, redirect_url
+
+app = FastAPI()
+print("main.py loaded")
+
+@app.post("/ShortenURL")
+def create_url_endpoint(url: str, db: Session = Depends(get_db)):
+    return create_url(url, db)
+
+
+@app.get("/{shortCode}")
+def redirect_to_originalUrl(shortcode: str, db: Session = Depends(get_db)):
+    return redirect_url(shortcode, db)
